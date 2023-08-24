51a3c445d06e19bad934ae3876599d10086482c0
unknown <samtisme@gmail.com>
2023-08-23 22:10:39 -0700

    added UserSchema

--- models/User.js
+++ models/User.js
@@ -1,14 +1,46 @@
 // import schema and model
+const { Schema, model } = require('mongoose');
 // import thoughts model
-
+const Thought = require('./Thought');
 // set up user schema
+const UserSchema = new Schema(
     // define fields
+    { 
+        username: {
     // username
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: { 
     // email
-    // password
-    // thoughts (array of _id values referencing the Thought model)
-    // friends (array of _id values referencing the User model)
+            type: String,
+            unique: true,
+            required: true,
+            // match a valid email address using regex to validate
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        }, // thoughts (array of _id values referencing the Thought model)
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],// friends (array of _id values referencing the User model)
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }
+);
     // use virtual to get friendCount
-// create the User model using the UserSchema
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 // get total count of friends on retrieval
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
 // export the User model
