adc538c1e38f4fc3ff3ccd446258cb7199e479d7
unknown <samtisme@gmail.com>
2023-08-25 10:05:09 -0700

    added thought routes edits

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -9,24 +9,31 @@ const router = express.Router();
 
 // route to get all thoughts
 router.route('/thoughts').get(thoughtController.getAllThoughts);
+// http://localhost:3001/api/thoughts
 
 // route to get one thought by id
 router.route('/thoughts/:id').get(thoughtController.getThoughtById);
+// http://localhost:3001/api/thoughts/:id
 
 // route to create a new thought
 router.route('/thoughts').post(thoughtController.createThought);
+// http://localhost:3001/api/thoughts
 
 // route to update a thought by id
 router.route('/thoughts/:id').put(thoughtController.updateThought);
+// http://localhost:3001/api/thoughts/:id
 
 // route to delete a thought by id
 router.route('/thoughts/:id').delete(thoughtController.deleteThought);
+// http://localhost:3001/api/thoughts/:id
 
 // route to add a reaction to a thought
 router.route('/thoughts/:id/reactions').post(thoughtController.addReaction);
+// http://localhost:3001/api/thoughts/:id/reactions
 
 // route to delete a reaction from a thought
 router.route('/thoughts/:thoughtId/reactions/:reactionId').delete(thoughtController.deleteReaction);
+// http://localhost:3001/api/thoughts/:id/reactions/:id
 
 // export router
 module.exports = router;
